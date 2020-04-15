5dc07cdf31f623f7f9e0ff73cf4be871a16e7336
jjerry-k <jaeyeol2931@gmail.com>
2020-04-15 22:02:45 +0900

    Update

---
+++ 189.rotate-array.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=189 lang=python3
+#
+# [189] Rotate Array
+#
+
+# @lc code=start
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k):
+            previous = nums[-1]
+            for j in range(len(nums)):
+                nums[j], previous = previous, nums[j]
+# @lc code=end
+
