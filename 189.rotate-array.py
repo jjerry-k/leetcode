1a5894d5227ed92e23af1fe4592edb3887fb1ec5
jjerry-k <jaeyeol2931@gmail.com>
2020-04-17 00:05:18 +0900

    Update

--- 189.rotate-array.py
+++ 189.rotate-array.py
@@ -2,17 +2,18 @@
 # @lc app=leetcode id=189 lang=python3
 #
 # [189] Rotate Array
-#
-
+# Your runtime beats 93.04 % of python3 submissions
+# Your memory usage beats 5.09 % of python3 submissions (15.4 MB)
 # @lc code=start
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(k):
-            previous = nums[-1]
-            for j in range(len(nums)):
-                nums[j], previous = previous, nums[j]
+        n = len(nums)
+        a = [0] * n
+        for i in range(n):
+            a[(i + k) % n] = nums[i]
+            
+        nums[:] = a
 # @lc code=end
-
