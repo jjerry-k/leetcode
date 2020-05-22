bc8bac8a3c80c8c707f6b905ac910d9cb4c32197
jjerry-k <jaeyeol2931@gmail.com>
2020-05-23 01:47:19 +0900

    Update

---
+++ 541.reverse-string-ii.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=541 lang=python3
+#
+# [541] Reverse String II
+# Your runtime beats 6.52 % of python3 submissions
+# Your memory usage beats 14.29 % of python3 submissions (13.9 MB)
+# @lc code=start
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        tmp = list(s)
+        for i in range(0, len(tmp), k*2):
+            tmp[i:i+k] = tmp[i:i+k][::-1]
+        return "".join(tmp)
+# @lc code=end
+
