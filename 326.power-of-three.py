6a96801fe1e74b80c3742824e66acd5d4b9c4cf3
Jerry Kim <jaeyeol2931@gmail.com>
2020-05-24 01:38:03 +0900

    Create 326.power-of-three.py

---
+++ 326.power-of-three.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=326 lang=python3
+#
+# [326] Power of Three
+# Your runtime beats 35.33 % of python3 submissions
+# Your memory usage beats 7.41 % of python3 submissions (13.9 MB)
+# @lc code=start
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n in (1, 3): return True
+        elif n % 3 or n < 1: return False
+        return self.isPowerOfThree(n // 3)
+# @lc code=end
