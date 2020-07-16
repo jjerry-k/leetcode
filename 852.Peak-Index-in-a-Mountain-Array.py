a452570534ceed52ad87fa7c0de95606a82f4c85
jjerry-k <jaeyeol2931@gmail.com>
2020-07-16 21:55:34 +0900

    Update

--- 852.Peak-Index-in-a-Mountain-Array.py
+++ 852.Peak-Index-in-a-Mountain-Array.py
@@ -1,6 +1,12 @@
-# Runtime: 76 ms, faster than 88.04% of Python3 online submissions for Peak Index in a Mountain Array.
-# Memory Usage: 15 MB, less than 58.11% of Python3 online submissions for Peak Index in a Mountain Array.
-
+#
+# @lc app=leetcode id=852 lang=python3
+#
+# [852] Peak Index in a Mountain Array
+# Your runtime beats 30.71 % of python3 submissions
+# Your memory usage beats 89.34 % of python3 submissions (14.9 MB)
+# @lc code=start
 class Solution:
     def peakIndexInMountainArray(self, A: List[int]) -> int:
         return A.index(max(A))
+# @lc code=end
+
