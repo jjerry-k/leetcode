b690bab75ade529ecfb626280cc1b4b7b301a8cf
jjerry-k <jaeyeol2931@gmail.com>
2020-11-21 01:41:59 +0900

    Update

---
+++ 1356.sort-integers-by-the-number-of-1-bits.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=1356 lang=python3
+#
+# [1356] Sort Integers by The Number of 1 Bits
+# Your runtime beats 91.92 % of python3 submissions
+# Your memory usage beats 20.79 % of python3 submissions (14.4 MB)
+# @lc code=start
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        return sorted(arr, key=lambda x: [bin(x).count("1"), x])
+# @lc code=end
