212f6c239abea65e5482c27f1abfb356d351eda3
jjerry-k <jaeyeol2931@gmail.com>
2021-01-26 01:34:32 +0900

    Update

---
+++ 912.sort-an-array.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=912 lang=python3
+#
+# [912] Sort an Array
+
+Your runtime beats 97.85 % of python3 submissions
+Your memory usage beats 55.72 % of python3 submissions (20.3 MB)
+# @lc code=start
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return sorted(nums)
+# @lc code=end
